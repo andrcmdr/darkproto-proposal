5e0e5e5486a21f61ac30dfb09c0234a46a94e54d
Andrew Bednoff <andrew@darkblock.io>
2022-09-10 01:37:59 +0600

    Improvements for Docker builds and run scripts.

--- .docker/docker.build.sh
+++ .docker/docker.build.sh
@@ -7,6 +7,15 @@ shopt -s extquote
 
 set -f
 
+### For cleaning up the Overlay FS (/var/lib/docker/overlay2)
+if [[ "$1" == "clean" ]]; then
+    docker image prune --all;
+    docker system prune --all --volumes;
+    docker volume prune;
+    docker builder prune --all;
+    DOCKER_BUILDKIT=1 docker buildx prune --all;
+fi
+
 docker build --no-cache -f ./enclave_base.dockerfile -t "enclave_base" ./
 
 DOCKER_BUILDKIT=1 docker build --no-cache -f ./cipher_benchmarks.dockerfile -t "cipher_benchmarks" ./
--- .docker/docker.run.sh
+++ .docker/docker.run.sh
@@ -7,15 +7,18 @@ shopt -s extquote
 
 set -f
 
-docker run cipher_benchmarks && docker logs --follow cipher_benchmarks
-# docker run cipher_benchmarks && docker logs --follow cipher_benchmarks 2>&1 | tee -a ./cipher_benchmarks.log
-# docker run cipher_benchmarks && docker logs --follow cipher_benchmarks >> ./cipher_benchmarks.log 2>&1
-# docker run -ti cipher_benchmarks bash
-# docker exec -ti cipher_benchmarks bash
+if [[ "$1" == "bench" ]]; then
+    docker run cipher_benchmarks && docker logs --follow cipher_benchmarks
+    # docker run cipher_benchmarks && docker logs --follow cipher_benchmarks 2>&1 | tee -a ./cipher_benchmarks.log
+    # docker run cipher_benchmarks && docker logs --follow cipher_benchmarks >> ./cipher_benchmarks.log 2>&1
+    # docker run -ti cipher_benchmarks bash
+    # docker exec -ti cipher_benchmarks bash
+fi
 
-docker run darkproto_app && docker logs --follow darkproto_app
-# docker run darkproto_app && docker logs --follow darkproto_app 2>&1 | tee -a ./darkproto_app.log
-# docker run darkproto_app && docker logs --follow darkproto_app >> ./darkproto_app.log 2>&1
-# docker run -ti darkproto_app bash
-# docker exec -ti darkproto_app bash
-
+if [[ "$1" == "app" ]]; then
+    docker run darkproto_app && docker logs --follow darkproto_app
+    # docker run darkproto_app && docker logs --follow darkproto_app 2>&1 | tee -a ./darkproto_app.log
+    # docker run darkproto_app && docker logs --follow darkproto_app >> ./darkproto_app.log 2>&1
+    # docker run -ti darkproto_app bash
+    # docker exec -ti darkproto_app bash
+fi
