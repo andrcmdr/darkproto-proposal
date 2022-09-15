54cf36bf4713e32b1e61e1b775dda9e9468a9c5b
Andrew Bednoff <andrew@darkblock.io>
2022-09-15 19:37:57 +0600

    Updated scripts for build and run benchmarks.

--- .docker/run.benchmarks.sh
+++ .docker/run.benchmarks.sh
@@ -29,50 +29,52 @@ openssl speed -elapsed -evp aes-256-cbc
 
 echo -e "\n"
 
-cd ./aes-primitives/
+cd /cipher-benchmarks/darkproto-proposal/aes-primitives/
 echo -e "$(pwd)\n"
-cargo +nightly bench -v --workspace --all-targets --all-features
+cargo +nightly bench -v --no-fail-fast --workspace --benches --all-features --frozen --locked --offline -Z unstable-options --keep-going
 
 echo -e "\n"
 
-cd ../aes-benchmarks/
+cd /cipher-benchmarks/darkproto-proposal/aes-benchmarks/
 echo -e "$(pwd)\n"
-cargo +nightly bench -v --workspace --all-targets --all-features
+cargo +nightly bench -v --no-fail-fast --workspace --benches --all-features --frozen --locked --offline -Z unstable-options --keep-going
 
 echo -e "\n"
 
-cd ../block-ciphers/
+cd /cipher-benchmarks/darkproto-proposal/block-ciphers/
 echo -e "$(pwd)\n"
-cargo +nightly bench -v --workspace --all-targets --all-features
+cargo +nightly bench -v --no-fail-fast --workspace --benches --all-features --frozen --locked --offline -Z unstable-options --keep-going
 
 echo -e "\n"
 
-cd ../AEADs/
+cd /cipher-benchmarks/darkproto-proposal/AEADs/
 echo -e "$(pwd)\n"
-cargo +nightly bench -v --workspace --all-targets --all-features
+cargo +nightly bench -v --no-fail-fast --workspace --benches --all-features --frozen --locked --offline -Z unstable-options --keep-going
 
 echo -e "\n"
 
-cd ../stream-ciphers/
+cd /cipher-benchmarks/darkproto-proposal/stream-ciphers/
 echo -e "$(pwd)\n"
-cargo +nightly bench -v --workspace --all-targets --all-features
+cargo +nightly bench -v --no-fail-fast --workspace --benches --all-features --frozen --locked --offline -Z unstable-options --keep-going
 
 echo -e "\n"
 
-cd ../RSA/
+cd /cipher-benchmarks/darkproto-proposal/RSA/
 echo -e "$(pwd)\n"
 patch --verbose --backup ./benches/key.rs ../.patches/RSA.key.rs.patch
-cargo +nightly bench -v --workspace --all-targets --all-features
+cargo +nightly bench -v --no-fail-fast --workspace --benches --all-features --frozen --locked --offline -Z unstable-options --keep-going
 
 echo -e "\n"
 
-cd ../elliptic-curves/
+cd /cipher-benchmarks/darkproto-proposal/elliptic-curves/
 echo -e "$(pwd)\n"
 patch --verbose --backup ./p256/benches/field.rs ../.patches/EC.p256.field.rs.patch
-cargo +nightly bench -v --workspace --all-targets --all-features
+cargo +nightly bench -v --no-fail-fast --workspace --benches --all-features --frozen --locked --offline -Z unstable-options --keep-going
 
 echo -e "\n"
 
-cd ../threshold-crypto/
+cd /cipher-benchmarks/darkproto-proposal/threshold-crypto/
 echo -e "$(pwd)\n"
-cargo +nightly bench -v --workspace --all-targets --all-features
+cargo +nightly bench -v --no-fail-fast --workspace --benches --all-features --frozen --locked --offline -Z unstable-options --keep-going
+
+echo -e "Successfully finished!!!\n"
---
+++ build.benchmarks.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#/usr/bin/env bash
+
+shopt -s extglob
+shopt -s extquote
+# shopt -s xpg_echo
+
+set -f
+
+echo -e "\n"
+
+cd /cipher-benchmarks/darkproto-proposal/aes-primitives/
+echo -e "$(pwd)\n"
+cargo +nightly build -v --workspace --benches --all-features --release -Z unstable-options --keep-going
+cargo +nightly bench -v --no-run --no-fail-fast --workspace --benches --all-features -Z unstable-options --keep-going
+
+echo -e "\n"
+
+cd /cipher-benchmarks/darkproto-proposal/aes-benchmarks/
+echo -e "$(pwd)\n"
+cargo +nightly build -v --workspace --benches --all-features --release -Z unstable-options --keep-going
+cargo +nightly bench -v --no-run --no-fail-fast --workspace --benches --all-features -Z unstable-options --keep-going
+
+echo -e "\n"
+
+cd /cipher-benchmarks/darkproto-proposal/block-ciphers/
+echo -e "$(pwd)\n"
+cargo +nightly build -v --workspace --benches --all-features --release -Z unstable-options --keep-going
+cargo +nightly bench -v --no-run --no-fail-fast --workspace --benches --all-features -Z unstable-options --keep-going
+
+echo -e "\n"
+
+cd /cipher-benchmarks/darkproto-proposal/AEADs/
+echo -e "$(pwd)\n"
+cargo +nightly build -v --workspace --benches --all-features --release -Z unstable-options --keep-going
+cargo +nightly bench -v --no-run --no-fail-fast --workspace --benches --all-features -Z unstable-options --keep-going
+
+echo -e "\n"
+
+cd /cipher-benchmarks/darkproto-proposal/stream-ciphers/
+echo -e "$(pwd)\n"
+cargo +nightly build -v --workspace --benches --all-features --release -Z unstable-options --keep-going
+cargo +nightly bench -v --no-run --no-fail-fast --workspace --benches --all-features -Z unstable-options --keep-going
+
+echo -e "\n"
+
+cd /cipher-benchmarks/darkproto-proposal/RSA/
+echo -e "$(pwd)\n"
+patch --verbose --backup ./benches/key.rs ../.patches/RSA.key.rs.patch
+cargo +nightly build -v --workspace --benches --all-features --release -Z unstable-options --keep-going
+cargo +nightly bench -v --no-run --no-fail-fast --workspace --benches --all-features -Z unstable-options --keep-going
+
+echo -e "\n"
+
+cd /cipher-benchmarks/darkproto-proposal/elliptic-curves/
+echo -e "$(pwd)\n"
+patch --verbose --backup ./p256/benches/field.rs ../.patches/EC.p256.field.rs.patch
+cargo +nightly build -v --workspace --benches --all-features --release -Z unstable-options --keep-going
+cargo +nightly bench -v --no-run --no-fail-fast --workspace --benches --all-features -Z unstable-options --keep-going
+
+echo -e "\n"
+
+cd /cipher-benchmarks/darkproto-proposal/threshold-crypto/
+echo -e "$(pwd)\n"
+cargo +nightly build -v --workspace --benches --all-features --release -Z unstable-options --keep-going
+cargo +nightly bench -v --no-run --no-fail-fast --workspace --benches --all-features -Z unstable-options --keep-going
+
+echo -e "Successfully finished!!!\n"
