329b25f1c763fefe7f86ff8bf33dff8de62ed2cd
Andrew Bednoff <andrew@darkblock.io>
2022-09-27 19:37:57 +0600

    Additional configuration parameters.

--- darkproto-enclave-streaming/src/config.rs
+++ darkproto-enclave-streaming/src/config.rs
@@ -3,9 +3,12 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct AppConfig {
     pub in_file: String,
-    pub out_file: String,
+    pub out_enc_file: String,
+    pub out_re_enc_file: String,
     pub init_enc_pwd_file: String,
     pub re_enc_pwd_file: String,
     pub init_random_seed_file: String,
     pub re_enc_random_seed_file: String,
+    pub enc_cipher_algo: String,
+    pub re_enc_cipher_algo: String,
 }
